a455647ab3a28ad848a61fa293a1a69d67b968f5
Kyle Siler <kylesiler@gmail.com>
2015-09-27 18:51:44 -0700

    Connect to Weather API

--- weather.js
+++ weather.js
@@ -2,6 +2,8 @@
 
 var Redis = require('redis');
 var redisClient = Redis.createClient();
+var credentials = require('./credentials.js');
+var http = require('http');
 
 redisClient.on("error", function(err) {
   console.log("Error starting redis " + err);
@@ -16,12 +18,28 @@ exports.retrieveByZip = function(request, reply, zip) {
   var zip = request.params.zip;
   redisClient.get(zip, function(err, redisReply) {
     if(redisReply) {
-      console.log("Reply is from cache");
-      reply(redisReply);
+        console.log("Reply is from cache");
+        reply(JSON.parse(redisReply)).header('X-Cached', true);
     } else {
-      console.log("Reply is from service");
-      redisClient.set(zip, "Future cached endpoint to get weather for " + zip);
-      reply("Future non-cached endpoint to get weather for " + zip);
+        console.log("Reply is from service");
+        getWeatherFromService(zip, reply);
     }
   });
-}
+};
+
+function getWeatherFromService(zip, reply) {
+  var options = {
+    host: 'api.openweathermap.org',
+    path: '/data/2.5/weather?zip=' + zip + ',us&AAPID=' + credentials.aapid
+  }
+  http.get(options, function(response) {
+    var str = '';
+    response.on('data', function(chunk) {
+      str += chunk;
+    });
+    response.on('end', function() {
+      redisClient.set(zip, str);
+      reply(JSON.parse(str)).header('X-Cached', false);
+    })
+  })
+};
